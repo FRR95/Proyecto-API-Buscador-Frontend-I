037f968d31ec7f321f4143fe813eeec08bf1d523
Fran <franrocher95@gmail.com>
2024-03-22 22:19:07 +0100

    feat:adding update user form in profile page

--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -1,13 +1,14 @@
 
 import "./CustomInput.css";
 
-export const CustomInput = ({className, type, placeholder, name, value, onChangeFunction, onBlurFunction}) => {
+export const CustomInput = ({className, type, placeholder, name, value, onChangeFunction, onBlurFunction,disabled}) => {
     
     return (
         <input 
             className={className}
             type={type}
             placeholder={placeholder}
+            disabled={disabled}
             name={name}
             value={value}
             onChange={onChangeFunction}
