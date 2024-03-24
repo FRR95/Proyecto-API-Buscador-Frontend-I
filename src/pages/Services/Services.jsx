bfd6377664c703563a99d78ad14e899a860ad0cb
Fran <franrocher95@gmail.com>
2024-03-24 13:21:15 +0100

    feat:implementing services data in services page

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,8 +1,52 @@
+import { useState } from "react"
+import { ServicesCard } from "../../components/ServicesCard/ServicesCard"
+import { GetServices } from "../../services/apiCalls"
 import "./Services.css"
+import { useEffect } from "react"
 export const Services=()=>{
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        if (services.length === 0) {
+          const BringData = async () => {
+            try {
+    
+    
+              const fetched = await GetServices()
+              setServices(fetched.data)
+    
+            } catch (error) {
+              console.log(error)
+            }
+          }
+          BringData()
+        }
+    
+      }, [services])
+
+
     return(
-        <div className="servicesDesign">
-        <div>Vista de Servicios</div>
-        </div>
+        <>
+        {
+            services.length > 0
+                ? (<div className="servicesDesign">
+
+                    {services.map(
+                        service => {
+                            return (
+                        
+                         
+                                    <ServicesCard
+                                    service_name={service.service_name}
+                                    description={service.description}
+                                    />
+                            )
+                        }
+                    )
+                    }
+                </div>)
+                : (<p>Los servicios estan viniendo </p>)
+        }
+        </>
     )
 }
