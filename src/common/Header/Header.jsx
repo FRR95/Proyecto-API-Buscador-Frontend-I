ecb3f553b254e116e288710ea26c12b328733d19
Fran <franrocher95@gmail.com>
2024-03-25 13:41:36 +0100

    feat:adding CustomLink component in Header

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,14 +1,38 @@
 
-
+import { useNavigate } from "react-router-dom";
 import "./Header.css"
+import { CustomLink } from "../../components/CustomLink/CustomLink";
 
 export const Header = () => {
+    const navigate = useNavigate();
+    const passport = JSON.parse(localStorage.getItem("passport"));
 
+    const logOut = () => {
+        localStorage.removeItem("passport");
+        navigate("/login");
+    };
 
     return (
         <div className="headerDesign">
 
-            HEADER
+            <CustomLink title={"home"} destination={"/"} />
+
+            {passport?.token ? (
+                <div className="menu">
+                    <CustomLink
+                        title={passport?.decodificado?.first_name}
+                        destination={"/profile"}
+                    />
+                    <div onClick={logOut}>
+                        <CustomLink title={"log out"} destination={"/"} />
+                    </div>
+                </div>
+            ) : (
+                <div className="menu">
+                    <CustomLink title={"register"} destination={"/register"} />
+                    <CustomLink title={"login"} destination={"/login"} />
+                </div>
+            )}
 
 
         </div>
